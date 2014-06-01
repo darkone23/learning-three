8b1dafd5a5f80bf2ece1d18c9e0d104e7857c35c
egghead <thomas.omans@gmail.com>
2014-06-01 14:55:06 +0000

    adding a history of where the walker travels :bullettrain_side:

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ require(
   ["three", "lib/scene", "lib/camera", "lib/sphere", "lib/renderer", "lib/walk"],
 
   function(three, scene, camera, sphere, renderer, walk) {
-    var app = document.getElementById("app");
+    var app = document.getElementById("app"),
+        circle = sphere.build(0x00539F);
     app.appendChild(renderer.domElement);
 
-    scene.add(sphere);
+    scene.add(circle);
     camera.position.z = 100;
 
     function render() {
       requestAnimationFrame(render);
-      walk(sphere);
+      var history = sphere.build(0x00539F);
+      scene.add(history);
+      walk(circle);
+      sphere.mirror(circle, history);
       renderer.render(scene, camera);
     };
     render();
